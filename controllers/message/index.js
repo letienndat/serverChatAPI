662dbc50d9c50df09c57041696494803e964cf61
letienndat <attn110601@gmail.com>
2024-06-20 16:57:15 +0700

    update deploy

--- controllers/message/index.js
+++ controllers/message/index.js
@@ -17,7 +17,7 @@ const savePathImage = `${parentDirectory}/images`;
 const savePathFile = `${parentDirectory}/files`;
 
 const { WebSocketServer } = require("ws");
-const sockserver = new WebSocketServer({ port: 8443 });
+const sockserver = new WebSocketServer({ port: 80 });
 const channels = new Map();
 
 sockserver.on("connection", (ws) => {
